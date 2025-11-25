92acd2d4ff7f9b9af2b9da2b6aa4bceb690e213c
Faizanahmed Saiyed <faizanahmed.s@devstree.in>
2025-11-25 11:37:33 +0530

    basic setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,48 @@
+import { Canvas } from '@react-three/fiber'
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
 
+// Creating a route for the "/" path and telling it to render the App component
 export const Route = createFileRoute('/')({
   component: App,
 })
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
+    // Canvas = the main 3D playground.
+    // Anything inside Canvas becomes part of the 3D scene.
+    <Canvas
+      // gl = WebGL settings.
+      // antialias: false → turns off smoothing for better performance.
+      gl={{ antialias: false }}
+      // dpr = Device Pixel Ratio.
+      // It controls how sharp the 3D rendering should be.
+      // [1, 1.5] → use 1 for normal screens, up to 1.5 for sharper displays
+      dpr={[1, 1.5]}
+    >
+      {/* 
+        mesh = a 3D object container.
+        Think of it like a box that holds:
+        - geometry (shape)
+        - material (color, texture)
+      */}
+      <mesh>
+        {/*
+          boxGeometry = defines the shape of the 3D object.
+          Here, it's simply a cube.
+        */}
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+
+      {/* Ambient light = base brightness
+          Without it, your objects will look too dark. */}
+      <ambientLight intensity={0.5} />
+
+      {/* 
+          To give the object shape, depth, and realism
+          To show where light is coming from
+      */}
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+    </Canvas>
   )
 }
